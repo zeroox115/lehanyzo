cab9587ac9e4444de1a5405844cbe57d811408d9
zeroox <zeroox@femboy.cx>
2024-02-22 21:11:17 +0100

    added get_color def and basic ping command lol

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+#get hex color code from env
+def get_color():
+    hex = os.getenv('HEX_COLOR_CODE')
+    return discord.Colour(int(hex, 16))
+
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = '!', intents = intents)
 
@@ -15,6 +20,17 @@ async def on_ready():
     await client.change_presence(activity=discord.Streaming(name="Twitch", url="https://www.twitch.tv/lehanyzo"))
     print('uwu')
 
+
+
+@client.command()
+async def ping(ctx):
+    color = get_color()
+    embed = discord.Embed(
+        title = 'Pong!',
+        description = f'A bot pingje: {round(client.latency * 1000)}ms',
+        color = color
+    )
+    await ctx.send(embed=embed)
 
 
 
