2a27ec4cd3d9d34576bb413903de64738a445109
zeroox <zeroox@femboy.cx>
2024-02-23 15:20:23 +0100

    main.py added cog loading + case insensitive arg set to True changed get_color and ping command footer + added custom lib import

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import discord 
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
+from lib import lehanyzo as lehanyzo
 
 #get hex color code from env
 def get_color():
-    hex = os.getenv('HEX_COLOR_CODE')
+    hex = lehanyzo.get.env_var('HEX_COLOR_CODE')
     return discord.Colour(int(hex, 16))
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix = '!', intents = intents)
+client = commands.Bot(command_prefix = '!', intents = intents, case_insensitive = True)
 
 @client.event
 async def on_ready(): 
     await client.change_presence(activity=discord.Streaming(name="Twitch", url="https://www.twitch.tv/lehanyzo"))
     print('uwu')
+    cogok = ['cogs.twitch', 'cogs.udvozlo', 'cogs.reakcio_rangok', 'cogs.embedsay']
+    for cog in cogok:
+        client.load_extension(cog)
+
 
 
 
@@ -30,6 +30,7 @@ async def ping(ctx):
         description = f'A bot pingje: {round(client.latency * 1000)}ms',
         color = color
     )
+    embed.set_footer(text=lehanyzo.get.kranem_footer(lehanyzo.get.env_var('FOOTER'), "ping"))
     await ctx.send(embed=embed)
 
 
